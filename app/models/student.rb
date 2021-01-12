e92c8914a5e37bd1189e8825ee9a05bd7d6b661e
Carlj0666 <carlj1881@yahoo.com>
2021-01-11 20:12:27 -0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,16 @@
 class Student < ActiveRecord::Base
-  
+
   def to_s
     self.first_name + " " + self.last_name
   end
 
-  def self.is_active
-    if @student.active != true
+  def is_active
+    if self.active == false
       puts "<%=link_to This student is currently inactive., student_path(student)%>"
-      @student.active = true
-    elsif @student.active != false
+      self.active = true
+    elsif @student.active == true
       puts "<%=link_to This student is currently active., student_path(student)%>"
-      @student.active = false
+      self.active = false
     end
   end
 
