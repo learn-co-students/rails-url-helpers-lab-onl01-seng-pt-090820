45c2f964b747ce269e091823df94c04375157d06
Carlj0666 <carlj1881@yahoo.com>
2021-01-11 19:29:39 -0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,17 @@
 class Student < ActiveRecord::Base
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def self.is_active
+    if @student.active != true
+      puts "<%=link_to This student is currently inactive., , student_path(student)%>"
+      @student.active = true
+    elsif @student.active != false
+      puts "<%=link_to This student is currently active., student_path(student)%>"
+      @student.active = false
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Student.destroy.all
+Student.create([{first_name: 'stoodent', last_name: '0'}, {first_name: 'stewdent', last_name: '1'} ,{first_name: 'student', last_name: '2'}, {first_name: 'studint', last_name: '3'}])
