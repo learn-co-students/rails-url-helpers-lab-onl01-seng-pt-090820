a5a83617c3a2ddd394608649068d83b3a5dff8aa
Carlj0666 <carlj1881@yahoo.com>
2021-01-12 16:56:56 -0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,14 +4,4 @@ class Student < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
 
-  def is_active
-    if self.active == false
-      puts "<%=link_to This student is currently inactive., student_path(student)%>"
-      self.active = true
-    elsif @student.active == true
-      puts "<%=link_to This student is currently active., student_path(student)%>"
-      self.active = false
-    end
-  end
-
 end
---
+++ notes.md
@@ -0,0 +1,6 @@
+created activate route
+
+Link to the activate route in the student show page
+on clicking, 
+the activate "view" should invoke the if avtive make inactive, if not active make active logic "is_active method"
+then redirect to th show page
